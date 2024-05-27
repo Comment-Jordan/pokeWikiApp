bda0c2ac87d8a0eeaa3cba9272a03493dae9fb90
JordanCommented <jordanperezbravo@gmail.com>
2024-05-26 20:58:20 -0600

    ordenando descripcion de tipos

--- src/pages/InfoTipoPokemon.jsx
+++ src/pages/InfoTipoPokemon.jsx
@@ -1,6 +1,8 @@
 
 import { useLocation } from 'react-router-dom';
 
+import { DEFINICION_TIPOS } from '../utils/Diccionario';
+
 export default function PaginaTipo(){
     return(
         <article>
